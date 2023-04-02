7b25cc418f00ea8171d51947822ae6c7c0e6c2b1
dwiwahyup <dwiwahyu1235@gmail.com>
2023-04-03 05:24:06 +0700

    tugas 3

---
+++ tugas3.js
@@ -0,0 +1,10 @@
+for (var i = 1; i <= 20; i++) {
+    if (i % 2 === 0) {
+        console.log(i, "adalah bilangan genap")
+
+    } else if (i % 2 === 1){
+        console.log(i, "adalah bilangan ganjil")
+    } else {
+        console.log(i)
+    }
+}
